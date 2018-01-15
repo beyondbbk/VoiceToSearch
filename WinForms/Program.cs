504c9223b69d95977cde3b89950a64a53ccc9c67
beyondbbk <yangcuit@sina.cn>
2018-01-15 14:45:30 +0800

    公司提交测试

--- WinForms/Program.cs
+++ WinForms/Program.cs
@@ -14,7 +14,7 @@ namespace WinForms
         [STAThread]
         static void Main()
         {
-            
+            //公司提交测试
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
